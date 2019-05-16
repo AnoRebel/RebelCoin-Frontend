26a5585b1148df994046975d7bd34aa8fd479d15
Ano Rebel <hacker4rebel@gmail.com>
2019-05-16 17:05:03 +0300

    Added progress bar

--- static/js/script.js
+++ static/js/script.js
@@ -6,13 +6,18 @@ $(function() {
     AJAX to the server and set the selected block
     */
     $('.card').bind('click', function () {
+        NProgress.start();
+        NProgress.configure({ minimum: 0.2 });
+        NProgress.inc(0.2);
         $.getJSON($BASE_URL + "_select_block", {
             block_id: $(this).data('value')
         }, function(data) {
             $('#transaction').text(data.result);
             console.log(data.result);
             location.reload();
+            NProgress.done();
         });
+        NProgress.done(true);
         return false;
     });
     /*
@@ -20,12 +25,17 @@ $(function() {
     everytime the input changes
     */
     $('#difficulty').on('input', function () {
+        NProgress.start();
+        NProgress.configure({ minimum: 0.2 });
+        NProgress.inc(0.2);
         $.getJSON($BASE_URL + "_set_settings", {
             difficulty: $(this).val(),
         }, function(data) {
-            $('#difficulty').val(data.difficulty);
+            // $('#difficulty').val(data.difficulty);
             // console.log(data.difficulty);
+            NProgress.done();
         });
+        NProgress.done(true);
         return false;
     });
     /*
@@ -33,12 +43,17 @@ $(function() {
     everytime the input changes
     */
     $('#miningReward').on('input', function () {
+        NProgress.start();
+        NProgress.configure({ minimum: 0.2 });
+        NProgress.inc(0.2);
         $.getJSON($BASE_URL + "_set_settings", {
             reward: $(this).val(),
         }, function(data) {
             $('#reward').val(data.reward);
             // console.log(data.reward);
+            NProgress.done();
         });
+        NProgress.done(true);
         return false;
     });
     // Another AJAX call to the server to remove the intro message
@@ -53,8 +68,10 @@ $(function() {
     // AJAX call to tell  the server to mine the pending transactions in the queue
     $('#minePendingTransactions').on('click', function () {
         $miningInProgress = true;
+        NProgress.start();
+        NProgress.configure({ minimum: 0.2 });
+        NProgress.inc(0.2);
         $( this ).toggle();
-        // $( this ).attr('hidden', $miningInProgress);
         $('#miningInProgress').attr('hidden', !$miningInProgress);
         $.ajax({
             url: $BASE_URL + "_mine_transactions",
@@ -66,10 +83,12 @@ $(function() {
                 $('#miningInProgress').attr('hidden', !$miningInProgress);
                 $('#minePendingTransactions').toggle();
                 // $('#minePendingTransactions').attr('hidden', $miningInProgress);
+                NProgress.done();
                 location.href = location.origin;
             },
             error: function (error) {
                 alert(error.responseText);
+                NProgress.done(true);
             }
         });
     });
